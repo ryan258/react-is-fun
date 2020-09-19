6ba2602c5d475af3c8a810af03a35c1a11f2c024
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-09-19 18:12:04 -0500

    Examples of a class vs functional component.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+let skiData = {
+  total: 50,
+  powder: 20,
+  backcountry: 10,
+  goal: 100,
+};
+
+/*class SkiDayCounter extends Component {
+  getPercent = (decimal) => `${decimal * 100}%`;
+  calcGoalProgress = (total, goal) => this.getPercent(total / goal);
+
+  render() {
+    const { total, powder, backcountry, goal } = this.props;
+    return (
+      <section>
+        <div>
+          <p>Total Days: {total}</p>
+        </div>
+        <div>
+          <p>Powder Days: {powder}</p>
+        </div>
+        <div>
+          <p>backcountry Days: {backcountry}</p>
+        </div>
+        <div>
+          <p>Goal: {goal}</p>
+        </div>
+        <div>
+          <p>Goal Progress: {this.calcGoalProgress(total, goal)}</p>
+        </div>
+      </section>
+    );
+  }
+}*/
+
+const getPercent = (decimal) => `${decimal * 100}%`;
+const calcGoalProgress = (total, goal) => getPercent(total / goal);
+
+const SkiDayCounter = ({ total, powder, backcountry, goal }) => {
+  return (
+    <section>
+      <div>
+        <p>Total Days: {total}</p>
+      </div>
+      <div>
+        <p>Powder Days: {powder}</p>
+      </div>
+      <div>
+        <p>backcountry Days: {backcountry}</p>
+      </div>
+      <div>
+        <p>Goal: {goal}</p>
+      </div>
+      <div>
+        <p>Goal Progress: {calcGoalProgress(total, goal)}</p>
+      </div>
+    </section>
+  );
+};
+
+render(
+  <SkiDayCounter
+    total={skiData.total}
+    powder={skiData.powder}
+    backcountry={skiData.backcountry}
+    goal={skiData.goal}
+  />,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
