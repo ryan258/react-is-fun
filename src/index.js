f6e8f4fe5b8061efcce6951f6008a0133829895b
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-09-21 08:26:06 -0500

    Add state to library app and make library component a class component.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import "./index.css";
 
-// 1.) The data exists in a database
 let bookList = [
   { title: "Hunger", author: "Roxane Gay", pages: 320 },
   { title: "The Sun Also Rises", author: "Ernest Hemingway", pages: 260 },
@@ -10,7 +9,6 @@ let bookList = [
   { title: "Cat's Cradle", author: "Kurt Vonnegut", pages: 304 },
 ];
 
-// 3.) Data is mapped into individual child components
 const Book = ({ title, author, pages }) => {
   return (
     <section>
@@ -21,21 +19,54 @@ const Book = ({ title, author, pages }) => {
   );
 };
 
-// 2.) Data is pumped into a container component
-const Library = ({ books }) => {
-  return (
-    <div>
-      {/* <Book title="Steal This Book" author="Anonymous" pages={400} /> */}
-      {books.map((book, index) => (
-        <Book
-          key={index}
-          title={book.title}
-          author={book.author}
-          pages={book.pages}
-        />
-      ))}
-    </div>
-  );
-};
+// 0.) It's a good idea to keep state in the root of the tree!
+// 1.) Refactor stateless component to a class component
+class Library extends Component {
+  // 6. Clean up constructor and use a one liner because we're using prevState
+  state = { open: false };
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     open: true,
+  //   };
+  //   // 3. Bind the method to make 'this' accessible in the custom method
+  //   this.toggleOpenClosed = this.toggleOpenClosed.bind(this);
+  // }
+
+  // 2. Add a method to change state with setState
+  // 7. Bind this since we're not using the constructor, arrow functions will automatically bind this
+  toggleOpenClosed = () => {
+    // this.setState({
+    //   open: !this.state.open,
+    // });
+    // (5. Use callback functions to track previous state because setState is asynchronous and this will ensure that no matter how long it takes, it will work as expected.)
+    this.setState((prevState) => ({
+      open: !prevState.open,
+    }));
+  };
+
+  render() {
+    console.log(this.state);
+    const { books } = this.props;
+
+    return (
+      <div>
+        <h1>The library is {this.state.open ? "Open!" : "Closed..."}</h1>
+        {/* 4. Use the custom method in a click handler */}
+        <button onClick={this.toggleOpenClosed}>
+          Click to {this.state.open ? "Close" : "Open"}
+        </button>
+        {books.map((book, index) => (
+          <Book
+            key={index}
+            title={book.title}
+            author={book.author}
+            pages={book.pages}
+          />
+        ))}
+      </div>
+    );
+  }
+}
 
 render(<Library books={bookList} />, document.getElementById("root"));
